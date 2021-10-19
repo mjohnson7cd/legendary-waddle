392ec3b0d4962858d12e62f87336e1436654b2d1
mjohnson7cd <61306576+mjohnson7cd@users.noreply.github.com>
2021-10-19 15:19:19 -0500

    fixed cards, fixed links, added imges, javascript

--- scripts/gameScript.js
+++ scripts/gameScript.js
@@ -53,16 +53,30 @@ $.each(games, function (index, item) {
 
   // create a brand new HTML element JUST with code
 
-var el = `<div class='card text-center mx-auto bg-dark blue' style="width: 200px"> \
-  <button class='cardOpen2 btn btn-outline-info'> ${ item.title } </button> \
-   <div class='cardHide'> \
-       <br> \
-       <a href="${ item.link }" target="_blank" class="link-info" >GAME LINK</a> \
-       <p class="text-white">Create by: ${ item.author } </p> \
-       <br> \
-       <button class='btn btn-outline-info cardClose2'>Close</button> \
+   var el = `<div style="width: 25%;">
+   <div class="card">
+       <img class="card-img-top" src="${ item.image }" alt="github profile image">
+       <div class="card-body text-center">
+           <p class="card-text">
+               Created by: ${ item.author }
+           </p>
+           <a href="${ item.link }" target = "_blank" class="btn btn-outline-primary btn-sm">
+               GAME LINK
+           </a>
+           <button class='btn btn-outline-info cardClose2'>${ item.title }</button>
+       </div>
    </div>
- </div>`; 
+ </div>`
+// var el = `<div class='card text-center mx-auto bg-dark blue' style="width: 200px"> \
+//   <button class='cardOpen2 btn btn-outline-info'> ${ item.title } </button> \
+//    <div class='cardHide'> \
+//        <br> \
+//        <a href="${ item.link }" target="_blank" class="link-info" >GAME LINK</a> \
+//        <p class="text-white">Create by: ${ item.author } </p> \
+//        <br> \
+//        <button class='btn btn-outline-info cardClose2'>Close</button> \
+//    </div>
+//  </div>`; 
 
  console.log(el); 
 
